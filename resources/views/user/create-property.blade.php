09be7112784d8fee54e337697fe6f84ceee11db3
Shanti <yfe466@inf.elte.hu>
2026-06-04 20:43:23 +0200

    setup react and vite

--- resources/views/user/create-property.blade.php
+++ resources/views/user/create-property.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.dashboard')
+@section('content')
+  <!-- Replace the Blade form with this: -->
+  <div id="property-wizard"
+       data-token="{{ auth()->user()->createToken('spa')->plainTextToken }}">
+  </div>
+@endsection
+@vite('resources/js/app.jsx')
 
 @section('title', 'Create Property')
 @section('page-title', 'Create New Property')
